505a3ca53ab07eb09a4e996f774ddddda9d99fff
Johan Cala Torra <47458063+JohanCalaT@users.noreply.github.com>
2026-05-09 16:50:51 +0200

    fix(infra): fix frontend port mapping, YARP service discovery scheme, and add Scalar to .NET APIs

--- src/FootballManagerApp/FootballManagerApp.Comments.API/Program.cs
+++ src/FootballManagerApp/FootballManagerApp.Comments.API/Program.cs
@@ -1,11 +1,10 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddHealthChecks();
 
@@ -13,11 +12,8 @@ var app = builder.Build();
 
 app.MapDefaultEndpoints();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+app.MapOpenApi();
+app.MapScalarApiReference();
 
 app.UseHttpsRedirection();
 
